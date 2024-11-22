780d65f80f00c36501a4c445312c4d5fe9ab1f98
Ian Smythe <ianbsmythe@googlemail.com>
2024-11-22 14:21:31 +0000

    start js functionality

---
+++ script.js
@@ -0,0 +1,12 @@
+// create function for button to create password
+// link in the button onClick
+// create variables for password length, characters, and password
+// create for loop to generate password
+// create variable for random characters
+// return password
+
+function renderPasswords() {
+    let characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+    let passwordLength = 13;
+    
+}
